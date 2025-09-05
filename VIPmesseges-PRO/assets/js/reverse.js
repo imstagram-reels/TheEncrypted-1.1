757021bf7bdc81868aab3754c87d75782457a5ae
lnstagram <0601014563aa@gmail.com>
2025-09-05 06:12:06 +0100

    Update reverse.js

--- VIPmesseges-PRO/assets/js/reverse.js
+++ VIPmesseges-PRO/assets/js/reverse.js
@@ -4,16 +4,27 @@ let decodedText = "";
 
       const morseToTextMap = {
         '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
-        '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',};
+        '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',
+      };
+
       let words = morseInput.split(" ");
 
       for (let code of words) {
         if (morseToTextMap[code]) {
           decodedText += morseToTextMap[code];
-        } else {
-          decodedText += "Your data is not supported. Please enter the code.";}}
-      window.analyze = analyze; 
+        }
+        else if (morseInput.trim() === "") {
+            document.getElementById("result").innerHTML = "Data entry is required to complete the process.";
+        }
+        else {
+          decodedText += "#"; 
+        }
+      }
+
+      console.log(decodedText)
+      window.analyze = analyze;
     }
+
 const results = document.getElementById("result");
 const analyzeBtn = document.getElementById("button-analyze-and-time"); 
 
@@ -22,16 +33,23 @@ analyzeBtn.addEventListener("click", function() {
   if (userId) {
     getUserById(userId);
   } else {
-    results.textContent = "⚠️ المرجو إدخال معرف المستخدم";}});
+    results.textContent = "Data entry is required to complete the process.";
+  }
+});
+
 function getUserById(userId) {
   const dbRef = ref(database);
   get(child(dbRef, 'users/' + userId))
     .then((snapshot) => {
       if (snapshot.exists()) {
         const user = snapshot.val();
-        results.innerHTML = (user.input);
+        results.innerHTML = `<strong></strong> ${user.input}`;
       } else {
-        results.textContent = "Server error. Please try again later.";}})
+        results.textContent = "Server error. Please try again later.";
+      }
+    })
     .catch((error) => {
       console.error(error);
-      results.textContent = "❌ حدث خطأ، تحقق من Console";});}
+      results.textContent = "❌ حدث خطأ، تحقق من Console";
+    });
+}
