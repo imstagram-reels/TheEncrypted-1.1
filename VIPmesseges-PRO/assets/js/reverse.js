1cc2e9d568f67309768723223ba38a2b6ec00f13
lnstagram <0601014563aa@gmail.com>
2025-09-05 04:41:12 +0100

    Update reverse.js

--- VIPmesseges-PRO/assets/js/reverse.js
+++ VIPmesseges-PRO/assets/js/reverse.js
@@ -3,56 +3,26 @@ let decodedText = "";
       const morseInput = document.getElementById('input').value.trim();
 
       const morseToTextMap = {
-        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e',
-        '..-.': 'f', '--.': 'g', '....': 'h', '..': 'i', '.---': 'j',
-        '-.-': 'k', '.-..': 'l', '--': 'm', '-.': 'n', '---': 'o',
-        '.--.': 'p', '--.-': 'q', '.-.': 'r', '...': 's', '-': 't',
-        '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x', '-.--': 'y',
-        '--..': 'z',
         '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
-        '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',
-        '/': ' ',
-        '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!',
-        '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':',
-        '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
-        '.-..-.': '"', '...-..-': '$', '.--.-.': '@'
-      };
-
+        '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',};
       let words = morseInput.split(" ");
 
       for (let code of words) {
         if (morseToTextMap[code]) {
           decodedText += morseToTextMap[code];
         } else {
-          decodedText += "#"; // رمز غير معروف
-        }
-      }
-
-      // document.getElementById("result").innerText = decodedText;
-      // console.log(decodedText)
-      window.analyze = analyze; // نخليها global باش onclick يقدر يناديها
+          decodedText += "Your data is not supported. Please enter the code.";}}
+      window.analyze = analyze; 
     }
-
-
-// userIdInput
-// getUserBtn
-// userData
-
-
-
 const results = document.getElementById("result");
-// const userIdInput = decodedText;
-const analyzeBtn = document.getElementById("button-analyze-and-time"); // زر جديد
+const analyzeBtn = document.getElementById("button-analyze-and-time"); 
 
 analyzeBtn.addEventListener("click", function() {
   const userId = decodedText.trim();
   if (userId) {
     getUserById(userId);
   } else {
-    results.textContent = "⚠️ المرجو إدخال معرف المستخدم";
-  }
-});
-
+    results.textContent = "⚠️ المرجو إدخال معرف المستخدم";}});
 function getUserById(userId) {
   const dbRef = ref(database);
   get(child(dbRef, 'users/' + userId))
@@ -61,12 +31,7 @@ function getUserById(userId) {
         const user = snapshot.val();
         results.innerHTML = (user.input);
       } else {
-        results.textContent = "Server error. Please try again later."
-;
-      }
-    })
+        results.textContent = "Server error. Please try again later.";}})
     .catch((error) => {
       console.error(error);
-      results.textContent = "❌ حدث خطأ، تحقق من Console";
-    });
-}
+      results.textContent = "❌ حدث خطأ، تحقق من Console";});}
